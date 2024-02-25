a17413ed6e23fd953923401114a83b3e29c82816
Tommy Nurwantoro <tommy.nurwantoro@gmail.com>
2024-02-25 21:44:12 +0700

    Change otherData to general zap field

--- loader.go
+++ loader.go
@@ -22,68 +22,38 @@ func Load(config Config) LoggerInterface {
 }
 
 // Debug logs a message at DebugLevel.
-func Debug(ctx context.Context, msg string, otherData ...interface{}) {
-	if otherData == nil {
-		singleton.Debug(ctx, msg)
-		return
-	}
-
-	singleton.Debug(ctx, msg, zap.Any("otherData", otherData))
+func Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	singleton.Debug(ctx, msg, fields...)
 }
 
 // Info logs a message at InfoLevel.
-func Info(ctx context.Context, msg string, otherData ...interface{}) {
-	if otherData == nil {
-		singleton.Info(ctx, msg)
-		return
-	}
-
-	singleton.Info(ctx, msg, zap.Any("otherData", otherData))
+func Info(ctx context.Context, msg string, fields ...zap.Field) {
+	singleton.Info(ctx, msg, fields...)
 }
 
 // Warn logs a message at WarnLevel.
-func Warn(ctx context.Context, msg string, otherData ...interface{}) {
-	if otherData == nil {
-		singleton.Warn(ctx, msg)
-		return
-	}
-
-	singleton.Warn(ctx, msg, zap.Any("otherData", otherData))
+func Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	singleton.Warn(ctx, msg, fields...)
 }
 
 // Error logs a message at ErrorLevel.
-func Error(ctx context.Context, msg string, err error, otherData ...interface{}) {
-	if otherData == nil {
-		singleton.Error(ctx, msg, err)
-		return
-	}
-
-	singleton.Error(ctx, msg, err, zap.Any("otherData", otherData))
+func Error(ctx context.Context, msg string, err error, fields ...zap.Field) {
+	singleton.Error(ctx, msg, err, fields...)
 }
 
 // Fatal logs a message at FatalLevel.
 //
 // The logger then calls os.Exit(1), even if logging at FatalLevel is
 // disabled.
-func Fatal(ctx context.Context, msg string, err error, otherData ...interface{}) {
-	if otherData == nil {
-		singleton.Fatal(ctx, msg, err)
-		return
-	}
-
-	singleton.Fatal(ctx, msg, err, zap.Any("otherData", otherData))
+func Fatal(ctx context.Context, msg string, err error, fields ...zap.Field) {
+	singleton.Fatal(ctx, msg, err, fields...)
 }
 
 // Panic logs a message at PanicLevel.
 //
 // The logger then panics, even if logging at PanicLevel is disabled.
-func Panic(ctx context.Context, msg string, err error, otherData ...interface{}) {
-	if otherData == nil {
-		singleton.Panic(ctx, msg, err)
-		return
-	}
-
-	singleton.Panic(ctx, msg, err, zap.Any("otherData", otherData))
+func Panic(ctx context.Context, msg string, err error, fields ...zap.Field) {
+	singleton.Panic(ctx, msg, err, fields...)
 }
 
 // TDR (Transaction Detail Request) consist of request and response log
